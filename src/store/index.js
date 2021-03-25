e8b96dae41a0d8125e4143f8827821c056bbc965
iamsurajdev <surajbiswas367@gmail.com>
2021-03-25 09:27:54 -0800

    done some changes and reduce node-sass version

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,6 @@ import { createStore, applyMiddleware, compose } from "redux";
 import createSagaMiddleware from "redux-saga";
 import rootReducer from "./reducers";
 import rootSaga from "./saga";
-rootSaga;
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
   ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
